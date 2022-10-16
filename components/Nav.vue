761d8f0aea14d1767ccc1f9bf1f0a8f78d04292e
Nikolaos Tsoukas <Nick-tsoukas>
2022-10-16 15:32:41 -0500

    chore: changing logo

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center h-[72px] px-4">
     <div class="flex-grow">
-      <nuxt-img src="/logo.svg" />
+      <nuxt-img src="/finallogo.svg" />
     </div>
     <nuxt-img src="/menu.svg" />
   </div>
