6182de5afc147272d2ca2047241a4585ab9c5031
Nikolaos Tsoukas <Nick-tsoukas>
2022-10-16 10:11:22 -0500

    chore: adding layout and home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,19 @@
 <template>
-  <Hero />
+  <div>
+    <Hero />
+    {{ data }}
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      data: {},
+    }
+  },
+  async fetch() {
+    this.data = await this.$strapi.find('test-data')
+  },
+}
 </script>
